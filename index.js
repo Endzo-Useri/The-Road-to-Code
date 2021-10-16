81c0a0026ea4fc17c2031c24587c9e5cded1522e
endzo-useri <coffeeblack7@protonmail.com>
2021-10-16 14:33:06 -0700

    Update site

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const score = document.getElementById("score")
 function createGrid() {
 	for (let i = 0; i < 100; i++) {
 		const square = document.createElement("div")
+		square.classList.add()
 		grid.appendChild(square)
 	}
 }
