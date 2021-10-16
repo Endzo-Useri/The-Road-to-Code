c1fd771475d6d32406e3e336e4c183503ee5a5ec
endzo-useri <coffeeblack7@protonmail.com>
2021-10-16 14:30:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const grid = document.querySelector(".grid")
 const startBtn = document.getElementById("start")
 const score = document.getElementById("score")
+
+function createGrid() {
+	for (let i = 0; i < 100; i++) {
+		const square = document.createElement("div")
+		grid.appendChild(square)
+	}
+}
