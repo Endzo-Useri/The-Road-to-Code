b01ed073f1bc9d9b4a525be27fe514841e72c4df
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-10-22 21:19:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const startBtn = document.getElementById("start")
 const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2, 1, 0]
+let direction = 1
 
 function createGrid() {
 	for (let i = 0; i < 100; i++) {
@@ -19,5 +20,7 @@ currentSnake.forEach(index => squares[index].classList.add("snake"))
 function move() {
 	const tail = currentSnake.pop()
 	squares[tail].classList.remove("snake")
+	currentSnake.unshift(currentSnake[0] + direction)
+	squares[currentSnake[0]].classList.add("snake")
 }
 move()
