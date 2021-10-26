8569c240483f50222a0bc91e3166520c5272ecc4
endzo-useri <coffeeblack7@protonmail.com>
2021-10-25 21:10:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
+let width = 10
 
 function createGrid() {
 	for (let i = 0; i < 100; i++) {
@@ -27,4 +28,19 @@ move()
 
 let timerId = setInterval(move, 1000)
 
-clearInterval(timerId)
+function control(e) {
+	if (e.keyCode === 39) {
+			console.log('right pressed')
+			direction = 1
+	} else if (e.keyCode === 38) {
+			console.log('up pressed')
+			direction = -width
+	} else if (e.keyCode === 37) {
+			console.log('left pressed')
+			direction = -1
+	} else if (e.keyCode === 40) {
+			console.log('down pressed')
+			direction = +width
+	}
+}
+document.addEventListener('keyup', control)
