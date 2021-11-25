17d466cbed2c02e49edd76be9a2a90e3af7ae164
endzo-useri <coffeeblack7@protonmail.com>
2021-11-25 13:25:05 -0800

    Update site

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const startBtn = document.getElementById("start")
 const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2, 1, 0]
-let direction = 1
-let width = 10
+// let direction = 1
+// let width = 10
 
 function createGrid() {
 	for (let i = 0; i < 100; i++) {
@@ -18,13 +18,13 @@ createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add("snake"))
 
-// function move() {
-// 	const tail = currentSnake.pop()
-// 	squares[tail].classList.remove("snake")
-// 	currentSnake.unshift(currentSnake[0] + direction)
+function move() {
+	const tail = currentSnake.pop()
+	squares[tail].classList.remove("snake")
+	currentSnake.unshift(currentSnake[0] + direction)
 // 	squares[currentSnake[0]].classList.add("snake")
-// }
-// move()
+}
+move()
 
 // let timerId = setInterval(move, 1000)
 
