ae61bdd7edbdf1fcee37ae6db1484eea6082375f
endzo-useri <coffeeblack7@protonmail.com>
2021-11-30 22:53:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -31,16 +31,16 @@ clearInterval(timerId)
 function control(e) {
 	if (e.keyCode === 39) {
 			console.log('right pressed')
-// 			direction = 1
+			direction = 1
 	} else if (e.keyCode === 38) {
 			console.log('up pressed')
-// 			direction = -width
+			direction = -width
 	} else if (e.keyCode === 37) {
 			console.log('left pressed')
-// 			direction = -1
+			direction = -1
 	} else if (e.keyCode === 40) {
 			console.log('down pressed')
-// 			direction = +width
+			direction = +width
 	}
 }
 document.addEventListener('keyup', control)
