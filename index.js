a69782fc3d7c4e97f3b9b0713dfa1c647a7bf1cf
endzo-useri <coffeeblack7@protonmail.com>
2021-12-01 20:32:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ currentSnake.forEach(index => squares[index].classList.add("snake"))
 
 function move() {
 	if (
-		(currentSnake[0] + width >= 100 && direction)
+		(currentSnake[0] + width >= 100 && direction === 10) ||
+		()
 	)
 
 
